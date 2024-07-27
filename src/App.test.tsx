23067eec7a48430e97e815abf12fab7bf1f50f9a
Costas Basdekis <costas.basdekis@gmail.com>
2024-07-27 10:25:12 +0100

    Track and show money production

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { MoneyProductionStepMap, createMoneyProductionStepMap } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("createMoneyProductionStepMap", () => {
+  const moneyProductionStepMap: MoneyProductionStepMap = createMoneyProductionStepMap();
+  it("assigns correct values", () => {
+    expect(moneyProductionStepMap[0]).toEqual(-10);
+    expect(moneyProductionStepMap[9]).toEqual(-1);
+    expect(moneyProductionStepMap[10]).toEqual(0);
+    expect(moneyProductionStepMap[11]).toEqual(1);
+    expect(moneyProductionStepMap[20]).toEqual(5);
+    expect(moneyProductionStepMap[30]).toEqual(10);
+    expect(moneyProductionStepMap[31]).toEqual(11);
+    expect(moneyProductionStepMap[32]).toEqual(11);
+    expect(moneyProductionStepMap[65]).toEqual(22);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import './App.css';
 
+const MinMoneyProductionLevel: number = -10;
+const MaxMoneyProductionLevel: number = 30;
+const MoneyProductionStepDescriptions: { moneyProductionLevelEnd: number, stepCount: number }[] = [
+  { moneyProductionLevelEnd: 0, stepCount: 1 },
+  { moneyProductionLevelEnd: 10, stepCount: 2 },
+  { moneyProductionLevelEnd: 20, stepCount: 3 },
+  { moneyProductionLevelEnd: 29, stepCount: 4 },
+  { moneyProductionLevelEnd: 30, stepCount: 3 },
+];
+
+// Get the money production from the step number
+type StepNumber = number
+type MoneyProductionLevel = number
+export type MoneyProductionStepMap = {[key: StepNumber]: MoneyProductionLevel}
+
+export function createMoneyProductionStepMap(): MoneyProductionStepMap {
+  const stepMap: MoneyProductionStepMap = {};
+  let moneyProductionLevelStart = MinMoneyProductionLevel;
+  let stepStart = 0;
+  for (const description of MoneyProductionStepDescriptions) {
+    const moneyProductionLevelEnd = description.moneyProductionLevelEnd;
+    const stepCount = description.stepCount;
+    for (let moneyProductionLevel = moneyProductionLevelStart ; moneyProductionLevel <= moneyProductionLevelEnd ; moneyProductionLevel += 1) {
+      for (let step = stepStart ; step < stepStart + stepCount ; step += 1) {
+        stepMap[step] = moneyProductionLevel;
+      }
+      stepStart = stepStart + stepCount;
+    }
+    moneyProductionLevelStart = moneyProductionLevelEnd + 1;
+  }
+  return stepMap;
+}
+
+const moneyProductionStepMap: MoneyProductionStepMap = createMoneyProductionStepMap();
+
 interface Player {
   index: number,
   money: number,
+  moneyProductionStep: number,
 }
 
 interface AppState {
@@ -20,10 +56,12 @@ class App extends React.Component<{}, AppState> {
       {
         index: 1,
         money: 17,
+        moneyProductionStep: 10,
       },
       {
         index: 2,
         money: 17,
+        moneyProductionStep: 10,
       },
     ],
   };
@@ -43,6 +81,7 @@ class App extends React.Component<{}, AppState> {
             <tr>
               <th>Player</th>
               <th>Money</th>
+              <th>Money production (step)</th>
               <th>Status</th>
             </tr>
             </thead>
@@ -51,6 +90,7 @@ class App extends React.Component<{}, AppState> {
               <tr>
                 <td>{player.index}</td>
                 <td>{player.money}</td>
+                <td>{moneyProductionStepMap[player.moneyProductionStep]} ({player.moneyProductionStep})</td>
                 <td>{currentPlayerIndex === player.index ? "Playing" : ""}</td>
               </tr>
             ))}
