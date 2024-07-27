e7984b3dfdc7364ef5c34cb2be0d8ec10fc156e0
Costas Basdekis <costas.basdekis@gmail.com>
2024-07-27 09:19:32 +0100

    Add current player and move history

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import './App.css';
 
 interface AppState {
-
+  currentPlayerIndex: number,
+  moveDescriptions: string[],
 }
 
 class App extends React.Component<{}, AppState> {
+  state = {
+    currentPlayerIndex: 1,
+    moveDescriptions: ["Game started"],
+  };
+
   render() {
+    const { currentPlayerIndex, moveDescriptions } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1>Bronze</h1>
+          <label>Current player: {currentPlayerIndex}</label>
+          <br/>
+          <ol>
+            {moveDescriptions.map(description => (
+              <li>{description}</li>
+            ))}
+          </ol>
         </header>
       </div>
     );
